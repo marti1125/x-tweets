d1ad892d2abec92e1aeff02685ea43ed00ea8f85
Willy Aguirre <marti1125@gmail.com>
2014-02-22 19:22:52 -0500

    update project

--- src/main.js
+++ src/main.js
@@ -3,12 +3,23 @@
   // Create your component here
   // http://x-tags.org/docs
   
-  xtag.register('x-stub', {
+  xtag.register('x-tweets', {
+    extends: 'div',
     lifecycle: {
-      created: function() {},
-      inserted: function() {},
-      removed: function() {},
-      attributeChanged: function() {}
+      created: function() {
+        // superinputs begin life knowing they're super.
+        this.value = 'super';
+      },
+      inserted: function() {
+        // superinputs announce their arrival in the DOM like a boss!
+        //alert("Yeah, that's right, superinput comin' thro'!");
+      },
+      removed: function() {
+
+      },
+      attributeChanged: function() {
+
+      }
     }, 
     events: { 
     
@@ -17,7 +28,10 @@
       
     }, 
     methods: {
-      
+      showUrlInTweet :function(tweet){
+        var url_regexp = /\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/gi;
+        return tweet.replace(url_regexp,"<a href='$1' class='tweetURL' target='_blank'>$1</a>");  
+      }
     }
   });
 
